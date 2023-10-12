a98755ec054cc4e552deafc1589a3c3ace9aa5ca
Byron <byronmarineroo@gmail.com>
2023-10-12 13:25:17 -0600

    primer commit

---
+++ Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontenAdministracion.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Tipo { get; set; }
+    }
+}
---
+++ Models/Juego.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontenAdministracion.Models
+{
+    public class Juego
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "La clasificacion es requerido")]
+        public int TipoGenerosId { get; set; }
+
+        [Required(ErrorMessage = "El titulo es requerido")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "La imagen es requerida")]
+        public string Img { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerida")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La plataforma es requerido")]
+        public string Plataforma { get; set; }
+
+        [Required(ErrorMessage = "el genero es requerido")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "el formato es requerido")]
+        public string Formato { get; set; }
+
+        [Required(ErrorMessage = "El formato es requerida")]
+        public string size { get; set; }
+
+        [Required(ErrorMessage = "La Fecha es requerida")]
+        public string Fecha { get; set; }
+
+        [Required(ErrorMessage = "La version es requerida")]
+        public string Version { get; set; }
+
+        public Categoria TipoGenero { get; set; }
+    }
+}
